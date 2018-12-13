ea20d5616cb075ddc25922ccb99b607190481ba2
Luis De Bello <luis.debello@mulesoft.com>
2018-12-13 07:54:24 -0800

    Loading profile from system

--- scripts/system.sh
+++ scripts/system.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Configure profile file
+cp system/.profile ~/.profile
+ln -s ~/.profile ~/.zprofile
+source ~/.profile
+
 # Adding JVMs
 jenv enable-plugin export
 jenv add `/usr/libexec/java_home -v '8'`
@@ -9,10 +14,6 @@ jenv global 1.8
 # Git configuration
 git config --global user.name "Luis De Bello"
 git config --global user.email luis.debello@mulesoft.com
-
-# Configure profile file
-cp system/.profile ~/.profile
-ln -s ~/.profile ~/.zprofile
 
 # Configure nvm
 nvm install 8.9.4
