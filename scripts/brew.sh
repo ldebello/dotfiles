d229469df1b9d7418e9717802a3a3a23df040d47
Luis De Bello <luis.debello@mulesoft.com>
2022-05-10 13:30:37 -0300

    Update brew.sh

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,37 +1,13 @@
 #!/bin/sh
 
-brew tap homebrew/services
-brew tap homebrew/cask-versions
-
 brew install jenv
-brew install nvm
 brew install maven
 brew install gpg
 brew install pass
 brew install lastpass-cli
-brew install jq
-brew install dockutil
-brew install xmlstarlet
-brew install antigen
-brew install httpie
-brew install watch
-brew install wget
-brew install sslscan
 
-brew install redis
-brew install postgresql
-brew install mysql
-
-brew cask install iterm2
-brew cask install slack
-brew cask install visual-studio-code
-brew cask install caffeine
-brew cask install intellij-idea
-brew cask install postman
-brew cask install mysqlworkbench
-brew cask install pgadmin4
-brew cask install licecap
-brew cask install mat
-brew cask install wireshark
-
-brew cask install the-unarchiver
+brew install --cask slack
+brew install --cask visual-studio-code
+brew install --cask intellij-idea
+brew install --cask postman
+brew install --cask the-unarchiver
