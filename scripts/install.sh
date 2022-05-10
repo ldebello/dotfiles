a2b98ce77e0d13902a650697148d4babcdcaa6cf
Luis De Bello <luis.debello@mulesoft.com>
2022-05-10 14:33:01 -0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -60,7 +60,7 @@ main() {
     rm -rf $DOTFILES
     
     info "Installing oh-my-zsh"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     success "oh-my-zsh installed"
   fi
 
